79718c9112c7f47ba6efcda2dab5ade331077379
Sofia Rojas <sofiarojasuptm@gmail.com>
2026-04-08 12:33:51 -0400

    guardando componente de error

---
+++ src/Error.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Error extends React.Component {
+
+    render(){
+        return(
+            <p>
+                Error: el codigo es incorrecto
+            </p>
+        )
+    }
+}
+
+export {Error}
